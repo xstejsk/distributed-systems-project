ed1a588e022faf889397212c33adaee969c9f6ca
Radim Stejskal <xstejsk@fi.muni.cz>
2024-06-08 18:59:11 +0200

    installed dotenv

--- monitor/src/service/baseApi.ts
+++ monitor/src/service/baseApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.BACKEND_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:8080",
 });
 
 export default axiosInstance;
