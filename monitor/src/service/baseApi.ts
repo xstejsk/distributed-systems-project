a5c31f0f48705d904786410f92a70d0d9d084e85
Radim Stejskal <xstejsk@fi.muni.cz>
2024-06-11 14:39:33 +0200

    added k8s infrastructure

--- monitor/src/service/baseApi.ts
+++ monitor/src/service/baseApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:8080",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://34.34.162.227:8080",
 });
 
 export default axiosInstance;
